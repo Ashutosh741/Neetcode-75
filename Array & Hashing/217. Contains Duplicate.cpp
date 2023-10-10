d9d2113a99bbe17dfce45399a9f8b1bde12b2f36
Ashutosh <106524453+Ashutosh741@users.noreply.github.com>
2023-10-11 04:59:41 +0530

    Update 217. Contains Duplicate.cpp

--- Array & Hashing/217. Contains Duplicate.cpp
+++ Array & Hashing/217. Contains Duplicate.cpp
@@ -10,3 +10,17 @@ public:
     }
 
 };
+
+
+//  in python using hashing 
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
